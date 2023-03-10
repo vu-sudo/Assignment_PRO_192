3916bf52b3910dd3c666f3b11225d2f4b04335dc
vuvudev <vuvo070403@gmail.com>
2023-03-10 23:05:20 +0700

    first-update-assign

---
+++ Assignment/src/Model/Employee.java
@@ -0,0 +1,61 @@
+package Model;
+
+public class Employee {
+	private String id;
+	private String name;
+	private Integer age;
+	private String email;
+	private String address;
+	private Long salary;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getSalary() {
+		return salary;
+	}
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+	
+	public Employee(String id, String name, Integer age, String email, String address, Long salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.email = email;
+		this.address = address;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", email=" + email + ", address=" + address
+				+ ", salary=" + salary + "]";
+	}
+}
