92286bac6f9920167d4c3af6b8113791654623c6
Khanh <hnbkhanh080803@gmail.com>
2023-03-10 23:30:54 +0700

    Khanh-update-menu

--- Assignment/src/View/Menu.java
+++ Assignment/src/View/Menu.java
@@ -1,5 +1,41 @@
 package View;
 
-public class Menu {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
+public abstract class Menu {
+	private Scanner sc = new Scanner(System.in);
+	private String title;
+	private ArrayList<String> options = new ArrayList<>();
+	
+	public Menu(String title, String[] op) {
+		this.title = title;
+		this.options.addAll(Arrays.asList(op));
+	}
+	
+	public void menuDisplay() {
+		System.out.println("----------------------------");
+		System.out.println(title);
+		System.out.println("----------------------------");
+		for (int i = 0; i<options.size(); i++) {
+			System.out.println((i+1) + " - " + options.get(i));
+		}
+		System.out.println("----------------------------");
+	}
+	
+	public int getChoice() {
+		menuDisplay();
+		int choice = sc.nextInt();
+		return choice;
+	}
+	
+	public void run() {
+		while (true) {
+			int choice = getChoice();
+			execute(choice);
+			if (choice>options.size()) break;
+		}
+	}
+	public abstract void execute(int n);
 }
