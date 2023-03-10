217ce1925aab3011927c13d8e4315374e3bf3f39
Khanh <hnbkhanh080803@gmail.com>
2023-03-11 00:43:26 +0700

    Khanh-update-Company-Management

--- Assignment/src/View/CompanyManagement.java
+++ Assignment/src/View/CompanyManagement.java
@@ -25,8 +25,18 @@ public class CompanyManagement extends Menu{
 
 	@Override
 	public void execute(int n) {
-		// TODO Auto-generated method stub
-		
+		switch(n) {
+		case 1: System.out.println("1"); break; //Display all employee.
+		case 2: System.out.println("2"); break; //Delete a employee by id
+		case 3: System.out.println("3"); break; //Update info a employee by id.
+		case 4: System.out.println("4"); break; //Find employee by salary.
+		case 5: System.out.println("5"); break; //Sort list of employee.
+		case 6: System.out.println("6"); break; //Enter list a employee.
+		case 7: System.out.println("7"); break; //Save list of employees to file.
+		case 8: System.out.println("8"); break; //Export three older employee with highest salary.
+		case 9: System.out.println("Program end! \nThank you for using my program");
+		default: System.exit(0);
+		}
 	}
 	
 }
