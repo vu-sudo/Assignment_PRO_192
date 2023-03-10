32289420010774086ab58d50b49efcd132ca1f93
vuvudev <vuvo070403@gmail.com>
2023-03-11 00:13:39 +0700

    Vu-update-CompanyManagement

--- Assignment/src/View/CompanyManagement.java
+++ Assignment/src/View/CompanyManagement.java
@@ -1,5 +1,32 @@
 package View;
 
-public class CompanyManagement {
+import Controller.CompanyService;
+
+//VuVu
+public class CompanyManagement extends Menu{
+	private CompanyService service = new CompanyService();
+	
+	static String[] menu = {
+			"Display all employee.",
+			"Delete a employee by id.",
+			"Update info a employee by id.",
+			"Find employee by salary.",
+			"Sort list of employee.",
+			"Enter list a employee.",
+			"Save list of employees to file.",
+			"Export three older employee with highest salary.",
+			"EXIT PROGRAM..."
+			};
+	
+	public CompanyManagement() {
+		super("COMPANY EMPLOYEES MANAGEMENT", menu);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(int n) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
--- Assignment/src/View/Main.java
+++ Assignment/src/View/Main.java
@@ -3,9 +3,8 @@ package View;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello anh Zu zu");
-		System.out.println("Hello anh Zu ");
+		CompanyManagement company = new CompanyManagement();
+		company.run();
 	}
 
 }
