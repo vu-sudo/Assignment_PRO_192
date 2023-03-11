c693209276984b8facae81d0a0a3d6dd3c50ce02
Kac213 <nguyenanhkiet1448@gmail.com>
2023-03-11 11:00:33 +0700

    kiet-update

--- Assignment/src/Model/AdministrativeStaff.java
+++ Assignment/src/Model/AdministrativeStaff.java
@@ -1,5 +1,22 @@
 package Model;
 
-public class AdministrativeStaff{
-	
+public class AdministrativeStaff extends Employee implements ModelInterface{
+
+	public AdministrativeStaff(String id, String name, Integer age, String email, String address, Long salary) {
+		super(id, name, age, email, address, salary);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "AdministrativeStaff [getId()=" + getId() + ", getName()=" + getName() + ", getAge()=" + getAge()
+				+ ", getEmail()=" + getEmail() + ", getAddress()=" + getAddress() + ", getSalary()=" + getSalary()
+				+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ "]";
+	}
+	@Override
+	public Long calculateInCome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
