9d5774dd9cf3d8d7c56f87f54c0306d8c5215409
vuvudev <vuvo070403@gmail.com>
2023-03-11 18:38:35 +0700

    update

--- Assignment/src/Controller/CompanyService.java
+++ Assignment/src/Controller/CompanyService.java
@@ -38,10 +38,10 @@ public class CompanyService implements ServiceInterface{
 					employeeList.add(new DepartmentManager(l[0], l[1], l[2], Integer.parseInt(l[3]), l[5], l[4], Long.parseLong(l[6]), Long.parseLong(l[7])));
 					
 				} else if (l[0].equals("Marketing staff")) {
-					employeeList.add(new MarketingStaff(l[1], l[2], Integer.parseInt(l[3]), l[5], l[4],Long.parseLong(l[6]), Integer.parseInt(l[7]), Double.parseDouble(l[8])));
+					employeeList.add(new MarketingStaff(l[0], l[1], l[2], Integer.parseInt(l[3]), l[5], l[4],Long.parseLong(l[6]), Integer.parseInt(l[7]), Double.parseDouble(l[8])));
 					
 				} else if (l[0].equals("Administrative staff")) {
-					employeeList.add(new AdministrativeStaff(l[1], l[2], Integer.parseInt(l[3]), l[5], l[4], Long.parseLong(l[6])));
+					employeeList.add(new AdministrativeStaff(l[0 ], l[1], l[2], Integer.parseInt(l[3]), l[5], l[4], Long.parseLong(l[6])));
 				}
 			}
 		} catch (Exception e) {
